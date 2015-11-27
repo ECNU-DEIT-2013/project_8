eeb785d8cca60f6b0b7375bd61e22e8a79ffb07a
潘丽源 <269883753@qq.com>
2015-11-27 08:47:48 +0800

    sever端多了一些route
    现在包括addMessage，login，stock
    即可以访问
    127.0.0.8:8008/login（POST）
    127.0.0.8:8008/stock
    127.0.0.8:8008/addmessage（这个需要用post方法访问）

--- bin/Sever.dart
+++ bin/Sever.dart
@@ -1,57 +1,65 @@
 import 'dart:io';
 import 'package:sqljocky/sqljocky.dart';
 import 'package:rest_frame/rest_frame.dart';
+import 'dart:convert';
 
+Router router1 = new Router();
+Router login = new Router();
+Router addMessage = new Router();
+var decoded;//用来接收client端发送的消息
 
-class UserList{
-  var Num,Name,Phone,Key;
-  var Age,Gender;
-  UserList(var a,var b,var c,var d,var e,var f){
-    Num=a;Name=b;Gender=c;Age=d;Phone=e;Key=f;
-  }
+main() async{
+  addMessage.post(postAddMessage, "/addmessage");
+  login.get(getLogin, "/login");
+  router1.get(getStock, "/stock");
+  var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8008);
+  listenForRequests(server);
 }
+
+String getStock() {
+  return "[rest_test ：) get stock]";
+  //你可以在这里添加各种数据操作，然后以json返回
+}
+
+String getLogin() {
+  String a = "[rest_test ：) get login]";
+  return a;
+  //你可以在这里添加各种数据操作，然后以json返回
+}
+
+postAddMessage( )async{//添加留言的函数
+  var pool = new ConnectionPool(host: '52.8.67.180', port: 3306, user: 'dec2013stu', password: 'dec2013stu', db: 'stu_10130340245');
+  print('adding...');
+  var addAdd = await pool.prepare('Insert into MessageList (UserNum,Message) values (?,?)');
+  await addAdd.execute(decoded);
+}
+
+void handleRequest(HttpRequest request,Router routen) {
+  routen.route(request);
+}
+
 void addCorsHeaders(HttpResponse res) {
   res.headers.add("Access-Control-Allow-Origin", "*");
   res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- }
-
-main() async {
-  var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8008);
-  print("Serving at ${server.address}:${server.port}");
-
-  await for (var request in server) {
-    addCorsHeaders(request.response);
-
-    print('Server is OK');
-    request.response
-      ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
-      ..write('test')
-      ..close();
-  }
 }
 
-getData(var host, var port, var user, var password, var db,var sql,var results,var resultString) async{
-  var pool = new ConnectionPool(host: host, port: port, user:user, password: password, db:db);
-  var results =await pool.query(sql);
+listenForRequests(HttpServer requests) async {
+  await for (HttpRequest request in requests){
+    addCorsHeaders(request.response);
+    print('accept');//测试是否接收到client端的request
 
-  results.forEach((row) {
-    resultString.add('"UserNum: ${row.UserNum};   UserName:${row.UserName};   UserPhone:${row[2]}"');
-     print('ok');
-   });
-}//显示数据
-
-InsertData(var sql,List a ) async{
-  var query1 = await pool.prepare(sql);
-  var result = await query1.execute(a);
-}//插入数据，在其他函数里调用的基础函数
-
-AddMessage(var sql) async{
-
-}//加留言的函数
-
-AddHeat() async{
-
-}//加热度的函数
-
+    if (request.uri.path=="/login"){
+      handleRequest(request,login);}//调用route为login的时候的函数
+    else if(request.uri.path=="/stock"){
+      handleRequest(request,router1);}//调用route为stock的时候的函数
+    else if(request.uri.path=="/addmessage"){
+      decoded = await request.transform(UTF8.decoder.fuse(JSON.decoder)).first;//解码client端send过来的一个List;
+      //decoded = await request.transform(UTF8.decoder).join();
+      //decoded = await request.transform(JSON.decoder).first;
+      print(decoded);
+      handleRequest(request,addMessage);//加留言的函数
+    }
+    else print("Can't find");}
+}
 
