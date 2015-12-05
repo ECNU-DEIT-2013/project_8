ac4af760a2d52f6293a7221d5127b7ebdfa7bb5c
KylinP <kkylin11@gmail.com>
2015-12-05 17:28:02 +0800

    星星评分
    终于写完这部分了，duang!duang!duang!
    另外，下拉列表更改课程后请用
    querySelector('#Stars').remove();
    
    querySelector('#RightBack').children.remove(querySelector('#Saymywords'));
    Loadmystar(mystarcount);
    Loadsaymywords();
    这四行代码重载评分星星和评教按钮，不然位置会乱掉

--- web/index.dart
+++ web/index.dart
@@ -6,6 +6,8 @@ import "dart:async";
 import "ClassData.dart";
 
 bool myorall;
+int mystarcount;
+
 void main() {
   querySelector('#Commit')              ///Commit为确认登录按钮
     ..onClick.listen(LogIn);            ///logIn()为按下确认键登录进入主页面的子函数
@@ -147,6 +149,10 @@ void LogIn(MouseEvent event){           ///登录按钮功能
   submitselect.text='查看评教';
   selects.children.add(submitselect);
 
+  mystarcount = 0;    ///这个整形为个人对某课程的评分，初始未评分为0
+  Loadmystar(mystarcount);           ///此处加载的是个人的评分星数
+
+
   DivElement saymywords = new DivElement();
   saymywords.id = 'Saymywords';
   saymywords.text = '我要评教';
@@ -167,6 +173,7 @@ void Classesshift(MouseEvent event){      ///切换至全部课程
     ..clear()
     ..add('Otherclassbt');
   querySelector('#Myclassbt').onClick.listen(Classesshift1);
+  querySelector('#Stars').remove();
 }
 
 void Classesshift1(MouseEvent event){     ///切换至我的课程
@@ -179,15 +186,323 @@ void Classesshift1(MouseEvent event){     ///切换至我的课程
     ..add('Otherclassbt1');
   querySelector('#Otherclassbt').onClick.listen(Classesshift);
   DivElement rightback = querySelector('#RightBack');
+
+  Loadmystar(mystarcount);       ///加载个人评星数
+
   if(myorall == false) {
-    DivElement saymywords = new DivElement();
-    saymywords.id = 'Saymywords';
-    saymywords.text = '我要评教';
-    saymywords.classes
-      ..clear()
-      ..add('Saymywords');
-    rightback.children.add(saymywords);
-    querySelector('#Saymywords').onClick.listen(addComments);
+    Loadsaymywords();
+
+  }
+
+
+}
+
+void Loadsaymywords(){
+  DivElement saymywords = new DivElement();
+  saymywords.id = 'Saymywords';
+  saymywords.text = '我要评教';
+  saymywords.classes
+    ..clear()
+    ..add('Saymywords');
+  querySelector('#RightBack').children.add(saymywords);
+  querySelector('#Saymywords').onClick.listen(addComments);
+}
+
+void Loadmystar(int mystarcount){
+  DivElement rightback = querySelector('#RightBack');
+  DivElement stars = new DivElement();  ///评分（包括“我的评分”文字的整个容器）
+  stars.id = 'Stars';
+  stars.classes
+    ..clear()
+    ..add('Stars');
+  rightback.children.add(stars);
+
+  DivElement starstext = new DivElement();    ///文字
+  starstext.id = 'Starstext';
+  starstext.text='我的评分';
+  starstext.classes
+    ..clear()
+    ..add('Starstext');
+  stars.children.add(starstext);
+
+  DivElement starscon= new DivElement();   ///星星容器
+  starscon.id='Starscon';
+  starscon.classes
+    ..clear()
+    ..add('Starscon');
+  stars.children.add(starscon);
+
+  DivElement star1 = new DivElement();
+  star1.id = 'Star1';
+  star1.text = '★';
+  starscon.children.add(star1);
+  Addstarspace();
+
+  DivElement star2 = new DivElement();
+  star2.id = 'Star2';
+  star2.text = '★';
+  starscon.children.add(star2);
+  Addstarspace();
+
+  DivElement star3 = new DivElement();
+  star3.id = 'Star3';
+  star3.text = '★';
+  starscon.children.add(star3);
+  Addstarspace();
+
+  DivElement star4 = new DivElement();
+  star4.id = 'Star4';
+  star4.text = '★';
+  starscon.children.add(star4);
+  Addstarspace();
+
+  DivElement star5 = new DivElement();
+  star5.id = 'Star5';
+  star5.text = '★';
+  starscon.children.add(star5);
+
+
+                            ///此处将数据库传到服务器传到客户端的评分数据赋给mystarcount变量，会自动调整评分的五角星
+  if(mystarcount==0){                 ///这里用评分数来判断是否完成评分
+                              ///如果未完成，则有鼠标经过的特效，如已评分，则将加载的评分数（1~5）
+                              ///比如评了4分，则调用Star4()；
+    star1.onMouseEnter.listen(Starin1);
+    star2.onMouseEnter.listen(Starin2);
+    star3.onMouseEnter.listen(Starin3);
+    star4.onMouseEnter.listen(Starin4);
+    star5.onMouseEnter.listen(Starin5);
+    star1.onMouseOut.listen(Starout1);
+    star2.onMouseOut.listen(Starout2);
+    star3.onMouseOut.listen(Starout3);
+    star4.onMouseOut.listen(Starout4);
+    star5.onMouseOut.listen(Starout5);
+    star1.onClick.listen(Clickstar1);
+    star2.onClick.listen(Clickstar2);
+    star3.onClick.listen(Clickstar3);
+    star4.onClick.listen(Clickstar4);
+    star5.onClick.listen(Clickstar5);
+
+  }else if(mystarcount==1){
+    Star1();
+  }else if(mystarcount==2){
+    Star2();
+  }else if(mystarcount==3){
+    Star3();
+  }else if(mystarcount==4){
+    Star4();
+  }else if(mystarcount==5){
+    Star5();
+  }
+}
+void Addstarspace() {
+  DivElement starscon = querySelector('#Starscon');
+  DivElement space = new DivElement();
+  space.classes
+    ..clear()
+    ..add('Space');
+  starscon.children.add(space);
+}
+
+void Starin1(MouseEvent event){
+  Star1();
+}
+
+void Starin2(MouseEvent event){
+  Star2();
+}
+
+void Starin3(MouseEvent event){
+  Star3();
+}
+
+void Starin4(MouseEvent event){
+  Star4();
+}
+
+void Starin5(MouseEvent event){
+  Star5();
+}
+
+void Starout1(MouseEvent event){
+  if(mystarcount==0) {
+    Star0();
   }
 }
 
+void Starout2(MouseEvent event){
+  if(mystarcount==0) {
+    Star0();
+  }
+}
+
+void Starout3(MouseEvent event){
+  if(mystarcount==0) {
+    Star0();
+  }
+}
+
+void Starout4(MouseEvent event){
+  if(mystarcount==0) {
+    Star0();
+  }
+}
+
+void Starout5(MouseEvent event){
+  if(mystarcount==0) {
+    Star0();
+  }
+}
+
+void Star0(){
+  querySelector('#Star1').classes
+    ..clear()
+    ..add('StarA');
+  querySelector('#Star2').classes
+    ..clear()
+    ..add('StarA');
+  querySelector('#Star3').classes
+    ..clear()
+    ..add('StarA');
+  querySelector('#Star4').classes
+    ..clear()
+    ..add('StarA');
+  querySelector('#Star5').classes
+    ..clear()
+    ..add('StarA');
+}
+
+void Star1(){
+  querySelector('#Star1').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star2').classes
+    ..clear()
+    ..add('StarA');
+  querySelector('#Star3').classes
+    ..clear()
+    ..add('StarA');
+  querySelector('#Star4').classes
+    ..clear()
+    ..add('StarA');
+  querySelector('#Star5').classes
+    ..clear()
+    ..add('StarA');
+}
+
+void Star2(){
+  querySelector('#Star1').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star2').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star3').classes
+    ..clear()
+    ..add('StarA');
+  querySelector('#Star4').classes
+    ..clear()
+    ..add('StarA');
+  querySelector('#Star5').classes
+    ..clear()
+    ..add('StarA');
+}
+
+void Star3(){
+  querySelector('#Star1').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star2').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star3').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star4').classes
+    ..clear()
+    ..add('StarA');
+  querySelector('#Star5').classes
+    ..clear()
+    ..add('StarA');
+}
+
+void Star4(){
+  querySelector('#Star1').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star2').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star3').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star4').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star5').classes
+    ..clear()
+    ..add('StarA');
+}
+
+void Star5(){
+  querySelector('#Star1').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star2').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star3').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star4').classes
+    ..clear()
+    ..add('StarB');
+  querySelector('#Star5').classes
+    ..clear()
+    ..add('StarB');
+}
+
+void Clickstar1(MouseEvent event){                ///点击星星评分的函数，对应的每个函数评分完成后请将mystarcount里的整形上传数据库！！
+  Star1();
+  mystarcount=1;
+  querySelector('#Stars').remove();
+  querySelector('#RightBack').children.remove(querySelector('#Saymywords'));
+  Loadmystar(mystarcount);
+  Loadsaymywords();
+}
+
+void Clickstar2(MouseEvent event){
+  Star2();
+  mystarcount=2;
+  querySelector('#Stars').remove();
+  querySelector('#RightBack').children.remove(querySelector('#Saymywords'));
+  Loadmystar(mystarcount);
+  Loadsaymywords();
+
+}
+
+void Clickstar3(MouseEvent event){
+  Star3();
+  mystarcount=3;
+  querySelector('#Stars').remove();
+  querySelector('#RightBack').children.remove(querySelector('#Saymywords'));
+  Loadmystar(mystarcount);
+  Loadsaymywords();
+}
+
+void Clickstar4(MouseEvent event){
+  Star4();
+  mystarcount=4;
+  querySelector('#Stars').remove();
+  querySelector('#RightBack').children.remove(querySelector('#Saymywords'));
+  Loadmystar(mystarcount);
+  Loadsaymywords();
+}
+
+void Clickstar5(MouseEvent event){
+  Star5();
+  mystarcount=5;
+  querySelector('#Stars').remove();
+  querySelector('#RightBack').children.remove(querySelector('#Saymywords'));
+  Loadmystar(mystarcount);
+  Loadsaymywords();
+}
